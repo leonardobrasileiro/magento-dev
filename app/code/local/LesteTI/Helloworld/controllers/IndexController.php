35b10790bd1b5a6ff53533cc7b373b9d3b63d02b
leonardo <leo@lesteti.com.br>
2016-07-24 10:54:31 -0300

    Using collections

--- app/code/local/LesteTI/Helloworld/controllers/IndexController.php
+++ app/code/local/LesteTI/Helloworld/controllers/IndexController.php
@@ -43,4 +43,31 @@ class LesteTI_Helloworld_IndexController extends Mage_Core_Controller_Front_Acti
         echo 'Success';
     }
 
+    /**
+     * Guia completo para uso de collections:
+     *
+     * https://magento2.atlassian.net/wiki/display/m1wiki/Using+Magento+1.x+collections
+     *
+     */
+    public function collectionAction() {
+        $productCollection = Mage::getModel('catalog/product')
+            ->getCollection()
+            ->addAttributeToSelect('price')
+            ->addAttributeToSelect('image')
+//            ->addAttributeToFilter('name', 'French Cuff Cotton Twill Oxford')
+//            ->addAttributeToFilter('entity_id', array('in' => array(624,625)))
+            ->addAttributeToFilter('name', array('like' => 'French%'))
+            ->setPageSize(10,1);
+
+        echo $productCollection->getSelect()->__toString();
+
+//      atualiza preço de todos os registros selecionados.
+//        $productCollection->setDataToAll('price',180.0);
+//        $productCollection->save();
+
+        foreach ($productCollection as $product) {
+            Zend_Debug::dump($product->debug());
+        }
+    }
+
 }
